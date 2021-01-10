d24d740f2d8c80bdc214b0441f85d8f31d6e3257
hugo <347501437@qq.com>
2021-01-10 20:38:51 +0800

    scend

---
+++ src/main/java/cn/itcast/Test.java
@@ -0,0 +1,13 @@
+package cn.itcast;
+
+/**
+ * description:
+ *
+ * @auther hugo
+ * @date 2021/1/10 20:37
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
